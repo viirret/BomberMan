b5edb3b7c68d98a40f9368d35149938e45418500
viirretvaltteri@gmail.com <viirretvaltteri@gmail.com>
2021-09-14 23:00:35 +0300

    upgrades to map

--- Assets/Scripts/MapDestroyer.cs
+++ Assets/Scripts/MapDestroyer.cs
@@ -5,27 +5,53 @@ using UnityEngine.Tilemaps;
 
 public class MapDestroyer : MonoBehaviour
 {
-    public Tilemap tilemap;
-    public Tile wallTile;
-    public Tile destructibleTile;
-    public GameObject explosionPrefab;
+    Tilemap tilemap;
+    Tile wallTile;
+    Tile destructibleTile;
+    GameObject explosionPrefab;
 
     // Unity doesn't allow to destroy Prefab
     GameObject temp;
+    
+    void Start()
+    {
+        // defining components
+        tilemap = GameObject.Find("TilemapTop").GetComponent<Tilemap>();
+        wallTile = Resources.Load<Tile>("GameTiles/Wall");
+        destructibleTile = Resources.Load<Tile>("GameTiles/Destructible");
+        explosionPrefab = Resources.Load<GameObject>("Explosion");
+    }
 
     public void Explode(Vector2 worldPos)
     {
         Vector3Int origincell = tilemap.WorldToCell(worldPos);
 
+        // standard values make these react to powerups
         ExplodeCell(origincell);
+        if(ExplodeCell(origincell + new Vector3Int(1, 0, 0)))
+        {
+            ExplodeCell(origincell + new Vector3Int(2, 0, 0));
+        }
+        if(ExplodeCell(origincell + new Vector3Int(0, 1, 0)))
+        {
+            ExplodeCell(origincell + new Vector3Int(0, 2, 0));
+        }
+        if(ExplodeCell(origincell + new Vector3Int(-1, 0, 0)))
+        {
+            ExplodeCell(origincell + new Vector3Int(-2, 0, 0));
+        }
+        if(ExplodeCell(origincell + new Vector3Int(0, -1, 0)))
+        {
+            ExplodeCell(origincell + new Vector3Int(0, -2, 0));
+        }
     }
 
-    void ExplodeCell(Vector3Int cell)
+    bool ExplodeCell(Vector3Int cell)
     {
         Tile tile = tilemap.GetTile<Tile>(cell);
 
         if(tile == wallTile)
-            return;
+            return false;
 
         if(tile == destructibleTile)
         {
@@ -36,6 +62,6 @@ public class MapDestroyer : MonoBehaviour
         Vector3 pos = tilemap.GetCellCenterWorld(cell);
         temp = Instantiate(explosionPrefab, pos, Quaternion.identity);
         Destroy(temp, 1);
-        
+        return true;        
     }   
 }
