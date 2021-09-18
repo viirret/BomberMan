87d3b900f2310fd2a823ee7e04ce44b260db0c9b
viirretvaltteri@gmail.com <viirretvaltteri@gmail.com>
2021-09-18 23:00:01 +0300

    all basics of dynamic map

--- Assets/Scripts/GameMap.cs
+++ Assets/Scripts/GameMap.cs
@@ -14,15 +14,39 @@ public class GameMap : MonoBehaviour
         Dirt = Resources.Load<Tile>("GameTiles/Dirt");
         Destructible = Resources.Load<Tile>("GameTiles/Destructible");
         
-        // creating grid and tilemaps
+        // creating grid and bottom tilemap
         var grid = new GameObject("Grid").AddComponent<Grid>();
-        var go = new GameObject("Tilemap");
-        var tm = go.AddComponent<Tilemap>();
-        var tr = go.AddComponent<TilemapRenderer>();
-        go.transform.SetParent(grid.transform);
-
-        tm.SetTile(new Vector3Int(0,0,0), Wall); // Or use SetTiles() for multiple tiles.
+        var go1 = new GameObject("TilemapBase");
+        var tm1 = go1.AddComponent<Tilemap>();
+        var tr1 = go1.AddComponent<TilemapRenderer>();
+        tr1.sortingOrder = 1;
         
+        // creating top tilemap with colliders
+        var go2 = new GameObject("TilemapTop");
+        var tm2 = go2.AddComponent<Tilemap>();
+        var tc2D = go2.AddComponent<TilemapCollider2D>();
+        var cc2D = go2.AddComponent<CompositeCollider2D>();
+        Rigidbody2D body = go2.GetComponent<Rigidbody2D>();
+        body.bodyType = RigidbodyType2D.Static;
+        var tr2 = go2.AddComponent<TilemapRenderer>();
+        tr2.sortingOrder = 2;
+
+        go1.transform.SetParent(grid.transform);
+        go2.transform.SetParent(grid.transform);
+
+        // making one map for now
+        SetBottonLayer();
+        SetTopLayer();
+    }
+
+    void SetBottonLayer()
+    {
+        //tm1.SetTile(new Vector3Int(0,0,0), Wall);
+
+    }
+
+    void SetTopLayer()
+    {
 
     }
 
