4ee2c6cebc6ad487d4627d3194fc755ce297be3b
viirretvaltteri@gmail.com <viirretvaltteri@gmail.com>
2021-09-22 12:16:05 +0300

    randomization of spawns

--- Assets/Scripts/CharacterSpawner.cs
+++ Assets/Scripts/CharacterSpawner.cs
@@ -5,21 +5,29 @@ using UnityEngine.Tilemaps;
 
 public class CharacterSpawner : MonoBehaviour
 {
+    // all the prefabs
     GameObject blueBird;
+    GameObject chicken;
+    GameObject eagle;
+    GameObject owl;
+    GameObject yellowBird;
 
+    List<Vector3> spawnPoints = new List<Vector3>(4);
     void Start()
     {
-        Vector3[] spawnPoints = new Vector3[4];
-        blueBird = Resources.Load<GameObject>("Blue bird");
-        blueBird.transform.localScale = new Vector3(0.06f, 0.04f, 1f);
         CreateSpawnPoints(spawnPoints);
         
-        GameObject player = CreateBird(blueBird, spawnPoints[0]);
-        PlayerController pc = player.AddComponent<PlayerController>();
-
+        blueBird = Resources.Load<GameObject>("Blue bird");
+        blueBird.transform.localScale = new Vector3(0.06f, 0.04f, 1f);
+        
+        // create the player first
+        int spawn1 = Random.Range(0, 4);
+        GameObject player = CreateBird(blueBird, spawnPoints[spawn1], false);
+        spawnPoints.RemoveAt(spawn1);
     }
 
-    GameObject CreateBird(GameObject bird, Vector3 spawn)
+    // making the bird
+    GameObject CreateBird(GameObject bird, Vector3 spawn, bool enemy)
     {
         GameObject obj;
         obj = Instantiate(bird, spawn, Quaternion.identity);
@@ -32,6 +40,14 @@ public class CharacterSpawner : MonoBehaviour
         body.gravityScale = 0;
         body.freezeRotation = true;
         BoxCollider2D bc = obj.AddComponent<BoxCollider2D>();
+        if(enemy)
+        {
+            // add the enemy controller
+        }
+        else
+        {
+            PlayerController pc = obj.AddComponent<PlayerController>();
+        }
         return obj;
     }
     
@@ -41,22 +57,11 @@ public class CharacterSpawner : MonoBehaviour
         
     }
 
-    void CreateSpawnPoints(Vector3[] spawnPoints)
+    void CreateSpawnPoints(List<Vector3> spawnPoints)
     {
-        spawnPoints[0].x = -8;
-        spawnPoints[0].y = -5.5f;
-        spawnPoints[0].z = 0;
-
-        spawnPoints[1].x = -8;
-        spawnPoints[1].y = 4.5f;
-        spawnPoints[1].z = 0;
-
-        spawnPoints[2].x = 8;
-        spawnPoints[2].y = -5.5f;
-        spawnPoints[2].z = 0;
-
-        spawnPoints[3].x = 8;
-        spawnPoints[3].y = 4.5f;
-        spawnPoints[3].z = 0;
+        spawnPoints.Add(new Vector3(-8, -5.5f, 0));
+        spawnPoints.Add(new Vector3(-8, 4.5f, 0));
+        spawnPoints.Add(new Vector3(8, -5.5f, 0));
+        spawnPoints.Add(new Vector3(8, 4.5f, 0));
     }
 }
