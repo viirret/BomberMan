c2a5615f6b7fa0ca6893f7f4849464c400d08c2e
viirretvaltteri@gmail.com <viirretvaltteri@gmail.com>
2021-11-19 14:24:35 +0200

    slider stuff

--- Assets/Scripts/Sliders.cs
+++ Assets/Scripts/Sliders.cs
@@ -6,6 +6,8 @@ using UnityEngine.Audio;
 
 public class Sliders : MonoBehaviour
 {
+    // set preferences from volume later
+    
     AudioMixer myMixer;
 
     // sliders
@@ -24,28 +26,45 @@ public class Sliders : MonoBehaviour
     float sfxSliderPercentage;
 
 
-    void Start()
+    void Awake()
     {
         myMixer = Resources.Load<AudioMixer>("myMixer");
 
+        //sliders
         gameSlider = GameObject.Find("gameSlider").GetComponent<Slider>();
         themeSlider = GameObject.Find("themeSlider").GetComponent<Slider>();
         sfxSlider = GameObject.Find("effectsSlider").GetComponent<Slider>();
+
+        // listeners
+        gameSlider.onValueChanged.AddListener(delegate { SetGameVolume(); });
+        themeSlider.onValueChanged.AddListener(delegate { SetThemeVolume(); });
+        sfxSlider.onValueChanged.AddListener(delegate { SetSfxVolume(); });
+
 
         // percentages
         gamePercent = GameObject.Find("gameSliderPercent").GetComponent<Text>();
         themePercent = GameObject.Find("themeSliderPercent").GetComponent<Text>();
         sfxPercent = GameObject.Find("effectsSliderPercent").GetComponent<Text>();
+
     }
 
-    public void SetGameVolume(float slider)
+    // this all could be made as one function. Might make it later
+    void SetGameVolume()
     {
-        Debug.Log("value changed");
-        gamePercent.text = Mathf.RoundToInt(slider * 100) + "%";
-        gameSliderPercentage = Mathf.RoundToInt(slider * 100);
+        gamePercent.text = Mathf.RoundToInt(gameSlider.value * 100) + "%";
+        myMixer.SetFloat("gameVolume", Mathf.Log10(gameSlider.value) * 20);
+    }
 
-        gameSliderPercentage = gameSlider.value;
-        myMixer.SetFloat("game", Mathf.Log10(slider) * 20);
+    void SetThemeVolume()
+    {
+       themePercent.text = Mathf.RoundToInt(themeSlider.value * 100) + "%";
+       myMixer.SetFloat("themeVolume", Mathf.Log10(themeSlider.value) * 20); 
+    }
+
+    void SetSfxVolume()
+    {
+        sfxPercent.text = Mathf.RoundToInt(sfxSlider.value * 100) + "%";
+        myMixer.SetFloat("sfxVolume", Mathf.Log10(sfxSlider.value) * 20);
     }
 
 }
